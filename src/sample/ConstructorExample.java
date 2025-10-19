e5285bc403feb76555befc7e543efb936eba8fc1
Abhinay07 <abhinaydoulagar07@gmail.com>
2025-10-19 22:22:49 +0530

    Initial Commit of files

--- src/sample/ConstructorExample.java
+++
@@ -1,2 +0,0 @@
-package src.sample;public class ConstructorExample {
-}
--- src/sample/Test2.java
+++
@@ -1,7 +0,0 @@
-package src.sample;
-
-public class Test2 {
-    static void main() {
-        System.out.printf("Hello");
-    }
-}
